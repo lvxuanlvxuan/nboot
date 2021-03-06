fedb543c9871304504fdf0143118e7cc0bdbd131
lvxuanlvxuan <m18105410261@163.com>
2021-03-06 21:28:08 +0800

    修改

--- src/main/java/com/nb/springboot/nboot/pojo/Person.java
+++ src/main/java/com/nb/springboot/nboot/pojo/Person.java
@@ -3,14 +3,10 @@ package com.nb.springboot.nboot.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
